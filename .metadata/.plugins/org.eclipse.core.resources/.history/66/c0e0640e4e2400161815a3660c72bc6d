ce7c8b5966573fcfd9c8dbc47c42f5abba785ffb
xiangdewei <yafengxiang@163.com>
2016-05-27 16:01:24 -0500

    change display to abstract in Duck class

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/66/c0e0640e4e2400161815a3660c72bc6d
@@ -0,0 +1,9 @@
+package w1_Duck;
+
+public class RubberDuck extends Duck{
+
+	RubberDuck(){
+		this.flyBehavior=new CanotFly();
+		this.quackBehavior=new Squeak();
+	}
+}
--- Lab5_Duck/src/w1_Duck/RubberDuck.java
+++ Lab5_Duck/src/w1_Duck/RubberDuck.java
@@ -1,9 +1,15 @@
 package w1_Duck;
 
-public class RubberDuck extends Duck{
+public class RubberDuck extends Duck {
 
-	RubberDuck(){
-		this.flyBehavior=new CanotFly();
-		this.quackBehavior=new Squeak();
+	RubberDuck() {
+		this.flyBehavior = new CanotFly();
+		this.quackBehavior = new Squeak();
 	}
+
+	@Override
+	public void display() {
+		System.out.println("\tdisplaying");
+	}
+
 }
