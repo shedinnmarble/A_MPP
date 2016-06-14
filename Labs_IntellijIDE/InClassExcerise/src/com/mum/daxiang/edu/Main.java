8aeb0ac86286b46e179f012b1cd5e0362fbc9a01
xiangdewei <yafengxiang@163.com>
2016-06-14 11:45:02 -0500

    add in class excerise

---
+++ Labs_IntellijIDE/InClassExcerise/src/com/mum/daxiang/edu/Main.java
@@ -0,0 +1,38 @@
+package com.mum.daxiang.edu;
+
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       // Main m=new Main();
+        String[] arr=new String[]{"1","2"};
+        System.out.println(countOccurrents(arr,"2"));
+        System.out.println(countOccurrents2(arr,"2"));
+
+    }
+    public static <T>  int countOccurrents(T[] arr,T target){
+        int count=0;
+        if(target==null){
+            for(T item:arr){
+                if(item==null)
+                    count++;
+            }
+        }else{
+            for(T item:arr){
+                if(target.equals(item)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static <T>  int countOccurrents2(T[] arr,T target){
+        return (int)Stream.of(arr).filter(x->{
+            if(target==null)
+                return x==target;
+            return x.equals(target);
+        }).count();
+    }
+}
